7815aa3dda9dce36d8682de9b55ce8e9ee37965a
t-kojima <t-kojima@sonicgarden.jp>
2019-05-22 08:30:54 +0900

    schedule function test

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+// const serviceAccount = require('./serviceAccount.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+// adminConfig.credential = admin.credential.cert(serviceAccount);
+// admin.initializeApp(adminConfig);
+// console.log(adminConfig)
+
+admin.initializeApp();
+
+exports.test = functions.https.onRequest((req, res) => {
+  res.send(functions.config());
+});
+
+exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun(() => {
+  console.log('This will be run every 1 minutes!');
+});
